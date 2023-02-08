c9c5d0927928740793275685f0ca289f5705648e
Grecco Oliva, Franco <srgrecco93@gmail.com>
2023-02-07 22:57:46 -0300

    feat(project): inject app instance to server instance

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ import configs from './configs';
 // Server
 import { Server } from './infrastructures/server/server';
 
+// Applications
+import app from './app';
+
 const server = new Server(configs.deployment);
 
+server.loadApp({ route: '/', app });
 server.start();
