34ca7d6ffad4aee365557310b6dcd781d6296aa3
Grecco Oliva, Franco <srgrecco93@gmail.com>
2023-02-07 22:40:08 -0300

    feat(tests): add Server.loadApp tests

--- tests/infrastructures/server/server.test.ts
+++ tests/infrastructures/server/server.test.ts
@@ -1,3 +1,7 @@
+// External modules
+import { Router } from 'express';
+import supertest from 'supertest';
+
 // Configs
 import configs from "../../../src/configs";
 
@@ -13,5 +17,29 @@ describe('Server', () => {
     it('Should load the server configs to the Server instance', () => {
         expect(server.configs).toBeDefined();
         expect(server.configs).toEqual(configs.deployment);
+    });
+
+    it('Should load the app instance on "/" route', async () => {
+        // define minimal app
+        const app = Router();
+        app.get('/', (_, res) => res.send('Hello World!'));
+
+        // load app into server "/" route
+        server.loadApp({ route: '/', app });
+        await server.start();
+
+        const request = supertest;
+        const url = `http://localhost:${configs.deployment.port}`;
+        const response = await request(url).get('/');
+
+        expect(response.statusCode).toBe(200);
+    });
+
+    it('Should fail for other routes', async () => {
+        const request = supertest;
+        const url = `http://localhost:${configs.deployment.port}`;
+        const response = await request(url).get('/other');
+
+        expect(response.statusCode).toBe(404);
     });
 })
