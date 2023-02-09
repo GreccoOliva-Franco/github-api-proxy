7002b60d35bb2d7f78cedb34cc3f884c03b8c044
Grecco Oliva, Franco <srgrecco93@gmail.com>
2023-02-09 17:27:12 -0300

    feat(app): add api routes

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+// External modules
+import { Router } from 'express';
+
+const router = Router();
+
+export default router;
--- src/app/index.ts
+++ src/app/index.ts
@@ -2,6 +2,8 @@
 import cors from 'cors';
 import express, { Router } from 'express';
 
+// Routers
+import apiRoutes from './app.routes';
 
 const app = Router();
 
@@ -9,5 +11,8 @@ const app = Router();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Load api routes
+app.use('/', apiRoutes);
 
 export default app;
